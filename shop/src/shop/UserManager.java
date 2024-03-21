bc12d0acae419b731640e5ace3713b6de8a990f0
kimseungwoo449 <ehdanf449@naver.com>
2024-03-21 15:47:23 +0900

    Add : createUser method

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -1,11 +1,13 @@
 package shop;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserManager {
 	private ArrayList<User> users;
 	private static UserManager instance = new UserManager();
-
+	private Scanner sc = new Scanner(System.in);
+	
 	private UserManager() {
 		users = new ArrayList<User>();
 		User admin = new User("admin","1111");
@@ -14,5 +16,44 @@ public class UserManager {
 
 	public static UserManager getInstance() {
 		return instance;
+	}
+	
+	private String inputString(String message) {
+		System.out.print(message + " : ");
+		return sc.next();
+	}
+	
+	private int inputNumber(String message) {
+		int number = -1;
+		try {
+			String input = sc.next();
+			number = Integer.parseInt(input);
+		} catch (Exception e) {
+			System.err.println("숫자로 입력가능합니다.");
+		}
+		return number;
+	}
+	
+	// Create User
+	public void createUser() {
+		String id = inputString("ID");
+		String password = inputString("PASSWORD");
+		
+		if(findUserById(id)!=null) {
+			System.err.println("이미 존재하는 ID입니다.");
+			return;
+		}
+		
+		User user = new User(id,password);
+		users.add(user);;
+		System.out.printf("회원가입 완료. %s님 안녕하세요\n",id);
+	}
+	
+	private User findUserById(String id) {
+		for(User user:users) {
+			if(user.getId().equals(id))
+				return user;
+		}
+		return null;
 	}
 }
