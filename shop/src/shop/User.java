f5383c2bc0d9fa768ab61bea506079071a6a8fd7
kimseungwoo449 <ehdanf449@naver.com>
2024-03-21 15:49:32 +0900

    Add : Class User clone Method

--- shop/src/shop/User.java
+++ shop/src/shop/User.java
@@ -1,19 +1,26 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class User {
 	private String id;
 	private String password;
-	
-	public User(String id,String password) {
+	private Cart myCart;
+
+	public User(String id, String password) {
 		this.id = id;
 		this.password = password;
 	}
-	
+
 	public String getId() {
 		return this.id;
 	}
-	
+
 	public String getPassword() {
 		return this.password;
+	}
+
+	public User clone() {
+		return new User(this.id, this.password);
 	}
 }
