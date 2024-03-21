624fa52113f4d948b75ba76e071709362a07ec0c
kimseungwoo449 <ehdanf449@naver.com>
2024-03-21 15:54:54 +0900

    Create : log

--- shop/src/shop/Shop.java
+++ shop/src/shop/Shop.java
@@ -1,10 +1,13 @@
 package shop;
 
 public class Shop {
+
 	private String shopName;
+	public static int log;
 	
 	public Shop(String shopName) {
 		this.shopName = shopName;
+		this.log = -1;
 	}
 	
 	public void run() {
