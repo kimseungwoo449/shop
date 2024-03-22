1d40c403040e85ed854b910fa61f44475f9a72a4
kimseungwoo449 <ehdanf449@naver.com>
2024-03-22 14:11:01 +0900

    Add : 생성자, getInstance

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -1,5 +1,39 @@
 package shop;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class FileManager {
+	private FileReader fr;
+	private FileWriter fw;
+	private BufferedReader br;
+
+	private File ItemFile;
+	private String ItemFileName;
+
+	private File UserFile;
+	private String UserFileName;
+
+	private static FileManager instance = new FileManager();
+
+	private UserManager userManager = UserManager.getInstance();
+	private ItemManager itemManager = ItemManager.getInstance();
+
+	private FileManager() {
+		ItemFileName = "item.txt";
+		ItemFile = new File(ItemFileName);
+		
+		UserFileName = "user.txt";
+		UserFile = new File(UserFileName);
+	}
+
+	public static FileManager getInstance() {
+		return instance;
+	}
+
+	public void save() {
+
+	}
 }
